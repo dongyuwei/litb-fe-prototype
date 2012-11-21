a18daddce0bb74536b04768ae842450b4130c0fe
dongyuwei <newdongyuwei@gmail.com>
2012-11-21 17:36:30 +0800

    add i18n demo

---
+++ README.md
@@ -0,0 +1,4 @@
+todo:
+ 1. dev tools for extracting,merging,compressing js and css.
+ 2. how to publish template to backend?
+ 3. howto publish frontend resources?
--- page/demo/demo.php
+++ page/demo/demo.php
@@ -3,7 +3,10 @@ require realpath(dirname(__FILE__).'/../../Mustache/Autoloader.php');
 Mustache_Autoloader::register();
 
 $mustache = new Mustache_Engine(array(
-    'template_class_prefix' => '__litb_',
+	'cache' => '/tmp/mustache/cache',
+    'helpers' => array('i18n' => function($text) {
+        echo $text;
+    }),
     'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__),array(
     	'extension' => '.html',
      )),
@@ -14,4 +17,24 @@ $mustache = new Mustache_Engine(array(
 
 $tpl = $mustache->loadTemplate('demo');
 echo $tpl->render();
+
+//start-------------------------i18n demo-------------------------------------------------------
+$m = new Mustache_Engine();
+
+$m->addHelper('i', function($text) {
+
+    $dictionary = array(
+        'Hello' => 'Hola',
+        'My name is {{ name }}.' => 'Me llamo {{ name }}.',
+    );
+
+    return array_key_exists($text, $dictionary) ? $dictionary[$text] : $text;
+});
+
+$tpl = $m->loadTemplate('{{#i}}Hello{{/i}}. {{#i}}My name is {{ name }}.{{/i}} {{#i18n}}test{{/i18n}}');
+echo $tpl->render(array(
+	'name' => 'Justin',
+	'Hello' => 'Hola',
+));
+//end-------------------------i18n demo-------------------------------------------------------
 ?>
